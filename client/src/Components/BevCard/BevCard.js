149175479502cb47de9c21634c0bcfbe4d4ae42c
Calli Bonnet <calli@paradox.ai>
2020-01-30 21:09:09 -0700

    added new tiles on the home page

--- client/src/Components/BevCard/BevCard.js
+++ client/src/Components/BevCard/BevCard.js
@@ -6,17 +6,16 @@ import "./BevCard.css";
 const BevCard = (props) => {
     return (
         <div className="bev-card col-md-2 text-center shadow">
-        
             <Card style={{ width: '24rem', marginBottom: '10px', border:'0px'}}>
-                <Card.Img class="coffee-image" variant="top" alt={props.name} src={props.image}></Card.Img>
+                <Card.Img className="coffee-image" variant="top" alt={props.name} src={props.image}></Card.Img>
                 <Card.Body>
                 <h3 class="coffee-name">{props.name}</h3>
                     <Card.Text>
                     <p class="coffee-type">{props.type}</p>
                     <p class="coffee-description">{props.description}</p>
-                    <p class="coffee-price">Price: ${props.price}.00 </p>
+                    <p class="coffee-price">${props.price}.00 </p>
                     </Card.Text>
-                    <Button class="coffee-order-btn" variant="danger" onClick={()=> props.addToCart(props.id)}>Add to Cart</Button>
+                    <Button class="coffee-order-btn" variant="outline-success" onClick={()=> props.addToCart(props.id)}>Add to Cart</Button>
                 </Card.Body>
             </Card>
         </div>
