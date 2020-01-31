0856e74a0da038aa7d5df82c42e31a19c1810d09
Calli Bonnet <calli@paradox.ai>
2020-01-30 18:29:29 -0700

    updated css on coffee page and home page

--- client/src/Components/BevList/BevList.js
+++ client/src/Components/BevList/BevList.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import "./BevList.css";
-import BevCard from "../../components/BevCard/BevCard";
+import BevCard from "../../Components/BevCard/BevCard";
 import drinks from "../../drinks.json";
-import Wrapper from "../../components/Wrapper/Wrapper";
+import Wrapper from "../../Components/Wrapper/Wrapper";
 
 class BevList extends Component {
     state = {
--- client/src/Components/Order/Order.js
+++ client/src/Components/Order/Order.js
@@ -1,6 +1,6 @@
 import React from "react"; 
 import "./Order.css"; 
-import BevList from "../../components/BevList/BevList"; 
+import BevList from "../../Components/BevList/BevList"; 
 
 const Order = () => {
     return (
