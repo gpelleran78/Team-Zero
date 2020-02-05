86211100134627ad8c5dbab08059e376fca95b48
gpelleran78 <geremy_pelleran@yahoo.com>
2020-02-04 21:34:29 -0700

    heroku fix

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Routing (API & view)
-app.use(routes);
+// app.use(routes);
 
 // Send every other request to the React app
 // Define any API routes before this runs
