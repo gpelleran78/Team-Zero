3bc31376d12ffd95bd8c03ac8785b5b6fb133f9b
gpelleran78 <geremy_pelleran@yahoo.com>
2020-02-06 18:24:26 -0700

    installed auth0

--- client/src/routes.js
+++ client/src/routes.js
@@ -4,7 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import Order from "./Components/Order/Order";
 import Listen from "./Components/Listen/Listen";
-import Events from "./Components/Events/Events";
+import EventPage from "./Components/EventPage/EventPage";
 import Login from "./Components/Login/Login";
 import Cart from "./Components/Cart/Cart"; 
 
@@ -14,7 +14,7 @@ function Routes() {
         <Switch>
                 <Route exact path="/" component={Home} />
                 <Route path="/order" component={Order} />
-                <Route path="/events" component={Events} />
+                <Route path="/eventpage" component={EventPage} />
                 <Route path="/listen" component={Listen} />
                 <Route path="/login" component={Login} />
                 <Route path="/cart" component={Cart} />
