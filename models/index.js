50b93dbe201bd14527a245a79e1b947f1e7df41f
Steven Bowman <stevenbowmanjr@gmail.com>
2020-02-11 10:46:01 -0700

    deploy

--- models/index.js
+++ models/index.js
@@ -7,7 +7,7 @@ var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'production';
 var config    = require(__dirname + '/../config/config.js')[env];
 var db        = {};
-process.env.JAWSDB_URL= mysql://'ecpgf3x66a3v1ql3:er000yeqxbj0t9it@ofcmikjy9x4lroa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/o74xv68ph7b7as0h'
+//process.env.JAWSDB_URL='mysql://ecpgf3x66a3v1ql3:er000yeqxbj0t9it@ofcmikjy9x4lroa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/o74xv68ph7b7as0h'
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
